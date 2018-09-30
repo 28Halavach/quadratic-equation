7bd7b0a5496aa985092dedd48d4afaa22f4bc67b
28Halavach <sasha2015to@gmail.com>
2018-09-30 20:51:48 +0300

    28Halavach solved the quadratic-equation 20180930

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	// your implementation
+
+	let array = equation.split(' ');
+
+	let a = array[0];
+
+	let b = parseInt(array[3] + array[4], 10);
+	let c = parseInt(array[7] + array[8], 10);
+	// где 10 основание системы счисления
+	// parseInt отсекает дробную часть числи
+
+	let Dis = Math.sqrt((b * b) - (4 * a * c));
+	let x1 = Math.round(((b - 2 * b) + Dis) / (2 * a));
+	let x2 = Math.round(((b - 2 * b) - Dis) / (2 * a));
+	// Метод Math.round() возвращает число, округлённое к ближайшему целому.
+	
+	if (x1 > x2){
+		return [x2,x1];
+	}
+	else{
+		return [x1,x2];
+	}
 }
